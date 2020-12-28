81f4f87cdd924362a524b719373d86a61afb86b6
retrickett <72355620+retrickett@users.noreply.github.com>
2020-12-28 13:38:56 -0500

    Add files via upload

---
+++ climbStairs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//You are climbing a staircase. It takes n steps to reach the top.
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+int climbStairs(int n){
+    int *a=(int *)malloc((n+1)*sizeof(int));
+    int i=0;
+    for(i=0;i<n+1;i++){
+        a[i]=0;
+    }
+    
+    if (n>0){
+        a[1]=1;
+    }
+    
+    if (n>1){
+        a[2]=2;
+        for (i=3;i<=n;i++){
+            a[i]=(a[i-1]+a[i-2]);
+        }
+    }
+
+    
+    return a[n];
+}
